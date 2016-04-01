93bf87826e7498550e0f04295843486f260290a1
emuschelli <emuschelli@yahoo.com>
2016-04-01 11:24:42 -0400

    Task 4 - VendingMachineTest created
    Task 4-   VendingMachineTest.java file created.

---
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package edu.towson.cis.cosc603.project4.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author emusc_000
+ *
+ */
+public class VendingMachineTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#VendingMachine()}.
+	 */
+	@Test
+	public void testVendingMachine() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#addItem(edu.towson.cis.cosc603.project4.vendingmachine.VendingMachineItem, java.lang.String)}.
+	 */
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#getItem(java.lang.String)}.
+	 */
+	@Test
+	public void testGetItem() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#removeItem(java.lang.String)}.
+	 */
+	@Test
+	public void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#insertMoney(double)}.
+	 */
+	@Test
+	public void testInsertMoney() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#getBalance()}.
+	 */
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#makePurchase(java.lang.String)}.
+	 */
+	@Test
+	public void testMakePurchase() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.towson.cis.cosc603.project4.vendingmachine.VendingMachine#returnChange()}.
+	 */
+	@Test
+	public void testReturnChange() {
+		fail("Not yet implemented");
+	}
+
+}
